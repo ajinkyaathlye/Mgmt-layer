4a150436d8f17cc0e3e261caa479670e53a176f8
Ajinkya Athlye <apocalyptic1994@gmail.com>
2017-02-08 12:04:03 +0530

    Improved and fixed modal UI

--- backup/utilsKB.py
+++ backup/utilsKB.py
@@ -2,13 +2,17 @@ import os
 import requests
 import json
 
+#ip="10.136.60.38"
 
-def get_token():
+def get_token(ip,username,password):
+
+	"""Get Openstack authentication token for all request opertaions"""
+
 	headers = {'Content-Type': 'application/json',}
 
-	data = '{"auth": {"tenantName": "demo", "passwordCredentials": {"username": "demo", "password":"root123"}}}'
+	data = '{"auth": {"tenantName": "demo", "passwordCredentials": {"username": "'+username+'", "password":"'+password+'"}}}'
 
-	response=requests.post('http://10.136.60.38:5000/v2.0/tokens', headers=headers, data=data)
+	response=requests.post('http://'+ip+':5000/v2.0/tokens', headers=headers, data=data)
 
 	parsed_response=json.loads(response.text)
 
@@ -17,117 +21,101 @@ def get_token():
 	#print token
 	return token
 
-def list_vms():
-	headers = {
-    'User-Agent': 'python-novaclient',
-    'Accept': 'application/json',
-    'X-OpenStack-Nova-API-Version': '2.25',
-    'X-Auth-Token': get_token(),}
-	response=requests.get('http://10.136.60.48:8774/v2.1/f8391ff92c6a4b45b0360f8c85eacb2f/servers/detail', headers=headers)
-	parsed_json_response=json.loads(response.text)
-	vmlist=[]
-	for i in range(len(parsed_json_response['servers'])):
-		vm=[]
-		vm.append(str(parsed_json_response['servers'][i]['id']))#ID
-		vm.append(str(parsed_json_response['servers'][i]['name']))#Name
-		vm.append(str( parsed_json_response['servers'][i]['status']))#Status
 		
-		
-		"""print parsed_json_response['servers'][i]['id']
-		print parsed_json_response['servers'][i]['name']
-		print parsed_json_response['servers'][i]['status']"""
-		if parsed_json_response['servers'][i]['OS-EXT-STS:power_state']==1:
-			vm.append("Running")#POWER_STATE
-			#print "Running"
-		else:
-			vm.append("OFF")#POWER_STATE
-			#print "OFF" 
-		if 'private' in parsed_json_response['servers'][i]['addresses']:
-			ipv4=parsed_json_response['servers'][i]['addresses']['private'][0]['addr']
-			ipv6=parsed_json_response['servers'][i]['addresses']['private'][1]['addr']
-			vm.append("Private "+str(ipv4)+" "+str(ipv6)) #NETWORK
-			#print "Private",ipv4," ",ipv6
-		else:
-			ipv4=parsed_json_response['servers'][i]['addresses']['public'][0]['addr']
-			ipv6=parsed_json_response['servers'][i]['addresses']['public'][1]['addr']
-			vm.append("Private "+str(ipv4)+" "+str(ipv6)) #NETWORK
-			#print "Public",ipv4," ",ipv6
-		#print vm
-		vmlist.append(vm)
-		#print "======================================================================="
-		
-	#print vmlist
-	return vmlist
-		
-def list_backups():
+def list_backups(ip, VMID):
+	#VMID='9580f46e-2bc3-42af-97bb-8a365ac9de71'
 	headers = {
     'User-Agent': 'python-novaclient',
     'Accept': 'application/json',
     'X-OpenStack-Nova-API-Version': '2.25',
     'X-Auth-Token': get_token(),
 	}
-	response=requests.get('http://10.136.60.48:8774/v2.1/f8391ff92c6a4b45b0360f8c85eacb2f/images/detail', headers=headers)
+	string='http://'+ip+':8774/v2.1/'+str(getProjectID())+'/images/detail'
+	#print string
+	response=requests.get(string, headers=headers)
 	parsed_json_response=json.loads(response.text)
 	backupList=[]
 	for i in range(len(parsed_json_response['images'])):
 		backup=[]
-		backup.append(str(parsed_json_response['images'][i]['id']))#ID
-		backup.append(str(parsed_json_response['images'][i]['name']))#Name
-		backup.append(str(parsed_json_response['images'][i]['status']))#Status
-		
-		
-		#print "ID",parsed_json_response['images'][i]['id']
-		#print "Name",parsed_json_response['images'][i]['name']
-		#print "Status",parsed_json_response['images'][i]['status']
 		if 'server' in parsed_json_response['images'][i]:
-			backup.append("VM ID"+str(parsed_json_response['images'][i]['server']['id']))#VM ID
+			if str(parsed_json_response['images'][i]['server']['id'])!= VMID:
+				continue
+			backup.append(str(parsed_json_response['images'][i]['id']))#ID
+			backup.append(str(parsed_json_response['images'][i]['name']))#Name
+			backup.append(str(parsed_json_response['images'][i]['status']))#Status
 		else:
-			backup.append("VM ID --")#VM ID
-			#print "VM ID","--"
+			continue
 		#print backup
-		#print "======================================================"
 		backupList.append(backup)
 	#print backupList
 	return backupList
-	
-	
-def backupvm():
+
+
+def getProjectID(ip):
+	headers = {'Content-Type': 'application/json',}
+
+	data = '{"auth": {"tenantName": "demo", "passwordCredentials": {"username": "admin", "password":"root123"}}}'
+
+	response=requests.post('http://'+ip+':5000/v2.0/tokens', headers=headers, data=data)
+
+	parsed_response=json.loads(response.text)
+
+	token=parsed_response['access']['token']['id']
+	headers = {
+    'User-Agent': 'python-keystoneclient',
+    'Accept': 'application/json',
+    'X-Auth-Token': token,}
+	response=requests.get('http://'+ip+':35357/v2.0/tenants', headers=headers)
+	parsed_json_response=json.loads(response.text)
+	#print parsed_json_response
+	gg=parsed_json_response['tenants']
+	for ll in gg:
+		if  ll['name']=='demo':
+			#print ll['id']
+			return ll['id']
+
+def getVMdetails():
+	#parameters: ip,user,pass,vmid
 	headers = {
     'User-Agent': 'python-novaclient',
-    'Content-Type': 'application/json',
     'Accept': 'application/json',
     'X-OpenStack-Nova-API-Version': '2.25',
     'X-Auth-Token': get_token(),
 }
 
-	data = '{"createBackup": {"backup_type": "daily", "rotation": "1", "name": "testbkup"}}'
+	response=requests.get('http://10.136.60.38:8774/v2.1/'+getProjectID()+'/servers/9580f46e-2bc3-42af-97bb-8a365ac9de71', headers=headers)
+	#print response.text
+	parsed_response=json.loads(response.text)
+	#print parsed_response
+	"""print parsed_response['server']['OS-EXT-AZ:availability_zone']
+	print parsed_response['server']['flavor']['id']
+	if ('private'in parsed_response['server']['addresses']):
+		print parsed_response['server']['addresses']['private'][1]['addr']
+	elif ('public'in parsed_response['server']['addresses']):
+		print parsed_response['server']['addresses']['public'][1]['addr']
+	print parsed_response['server']['security_groups'][0]['name']"""
+	
+	dict={}
+	dict['availability_zone']=parsed_response['server']['OS-EXT-AZ:availability_zone']
+	dict['flavorRef']=parsed_response['server']['flavor']['id']
+	if ('private'in parsed_response['server']['addresses']):
+		dict['uuid']=parsed_response['server']['addresses']['private'][1]['addr']
+	elif ('public'in parsed_response['server']['addresses']):
+		dict['uuid']= parsed_response['server']['addresses']['public'][1]['addr']
 
-	response=requests.post('http://10.136.60.48:8774/v2.1/f8391ff92c6a4b45b0360f8c85eacb2f/servers/ff587d41-0c91-4648-81e6-d74fa6f34615/action', headers=headers, data=data)
-	print response.text
-	print response
+	dict['security_groups']=parsed_response['server']['security_groups'][0]['name']
+	return dict
 	
 
-def restoreVM():
-	headers = {
-    'User-Agent': 'python-novaclient',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'X-OpenStack-Nova-API-Version': '2.25',
-    'X-Auth-Token': 'c56111d47341486fac961de01cc04846',
-}
+def main():
+	getVMdetails()
+	#print get_token()	
+	#list_vms()
+	list_backups()
+	#backupvm()
+	#deleteBackup()
+	#print getBackupIDbyName("Wed Feb 01 2017 12:53:50 GMT+0530 (IST)")
+	#list_backups()
 
-	data = '{"server": {"name": "testcode", "imageRef": "fe800404-88cf-4e03-b2df-888695f79ba2", "availability_zone": "nova", "flavorRef": "1", "max_count": 1, "min_count": 1, "networks": [{"uuid": "140687a0-dd64-4a98-839c-0b032d2c07c2"}], "security_groups": [{"name": "default"}]}}'
-
-	response=requests.post('http://10.136.60.48:8774/v2.1/f8391ff92c6a4b45b0360f8c85eacb2f/servers', headers=headers, data=data)
-	parsed_json_response=json.loads(response.text)
-	print "Backup restoring with id : ",parsed_json_response['server']['id']
-#list_backups()
-
-def main():		
-	return list_backups()
-#backupvm()
-
-
-#list_backups()
-
-#restoreVM()
+	#restoreVM()
+	#list_vms()
